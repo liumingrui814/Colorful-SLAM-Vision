b7a72386ac2fd0bbf04fabdcecdbe61effc433bb
liumingrui814 <1224020182@qq.com>
2024-07-17 17:34:57 +0800

    add optic flow and orientation

--- pseudoColor.cpp
+++ pseudoColor.cpp
@@ -44,6 +44,7 @@ cv::Mat PseudoColor::visualizeRangeImage(cv::Mat rangeMat, float nan_value, cv::
 }
 
 
+//-- 生成一个伪彩色的colorBar, 大小为 256x1
 cv::Mat PseudoColor::generateColorMap(cv::ColormapTypes mapType){
     cv::Mat valueTabel(256,1,CV_8UC1);
     cv::Mat ColorTabel;
@@ -54,4 +55,94 @@ cv::Mat PseudoColor::generateColorMap(cv::ColormapTypes mapType){
     //-- 用灰度图生成伪彩色的渲染图
     cv::applyColorMap(valueTabel,ColorTabel,mapType);
     return ColorTabel;
+}
+
+
+//-- 可视化一个边长(radius*2+1)正方形的光流颜色映射图
+cv::Mat OpticFlowColor::generatePixel2DViewMap(int radius)
+{
+    int img_width = radius * 2 + 1;
+    cv::Mat opticMap(img_width, img_width, CV_8UC3, cv::Scalar::all(0));
+    cv::cvtColor(opticMap, opticMap, cv::COLOR_BGR2HSV);
+    double max_norm = sqrt(2) * radius;
+    //-- i是行索引(y方向索引)
+    for(int i = 0; i < img_width; ++i)
+    {
+        //-- j是列索引(x方向索引)
+        for(int j = 0; j < img_width; ++j)
+        {
+            //-- 获得[i,j]位置对应的光流向量[x,y]
+            int x = j - radius;
+            int y = i - radius;
+            //-- 获得光流向量[x,y]的atan2角度, 取值范围为[0.2pi]
+            double theta = atan2(y,x);
+            if(theta < 0) theta += 2*M_PI;
+            //-- 获得光流向量[x,y]的模长
+            double norm = sqrt(x*x + y*y);
+            //-- 计算角度对应的HSV的H值
+            int h_val = cvRound(90.0*theta/M_PI);
+            //-- 计算模长对应的HSV的SV值
+            int s_val = cvRound(double(norm) / double(max_norm) * 255);
+            opticMap.at<cv::Vec3b>(i,j) = cv::Vec3b(h_val, s_val, 255);
+        }
+    }
+    cv::cvtColor(opticMap, opticMap, cv::COLOR_HSV2BGR);
+    return opticMap;
+}
+
+
+//-- 根据最大的光流模长生成一个二维向量的颜色
+cv::Vec3b OpticFlowColor::pixel2valueHSV(cv::Vec2f pixel, float maxRadius)
+{
+    float x = pixel[0];
+    float y = pixel[1];
+    //-- 获得光流向量[x,y]的atan2角度, 取值范围为[0.2pi]
+    double theta = atan2(y, x);
+    if(theta < 0) theta += 2*M_PI;
+    //-- 获得光流向量[x,y]的模长
+    double norm = sqrt(x*x + y*y);
+    //-- 计算角度对应的HSV的H值
+    int h_val = cvRound(90.0*theta/M_PI);
+    //-- 计算模长对应的HSV的SV值
+    int s_val = cvRound(double(norm) / double(maxRadius) * 255);
+    cv::Vec3b hsv_val = cv::Vec3b(h_val, s_val, 255);
+    return hsv_val;
+}
+
+
+//-- 使用HSV色表渲染一个二维光流图
+cv::Mat OpticFlowColor::visualizePixel2DMap(cv::Mat matPixel2D)
+{
+    cv::Mat vizMat(matPixel2D.rows, matPixel2D.cols, CV_8UC3, cv::Scalar::all(0));
+    //-- 首先寻找最大的norm
+    float maxRadius = -1;
+    for(int i = 0; i < matPixel2D.rows; ++i){
+        for(int j = 0; j < matPixel2D.cols; ++j){
+            cv::Vec2f val = matPixel2D.at<cv::Vec2f>(i,j);
+            float radius = sqrt(val[0] * val[0] + val[1] * val[1]);
+            if(radius > maxRadius) maxRadius = radius;
+        }
+    }
+    //-- 根据最大的radius渲染颜色
+    for(int i = 0; i < matPixel2D.rows; ++i){
+        for(int j = 0; j < matPixel2D.cols; ++j){
+            cv::Vec2f val = matPixel2D.at<cv::Vec2f>(i,j);
+            cv::Vec3b hsv_val = pixel2valueHSV(val, maxRadius);
+            vizMat.at<cv::Vec3b>(i,j) = hsv_val;
+        }
+    }
+    cv::cvtColor(vizMat, vizMat, cv::COLOR_HSV2BGR);
+    return vizMat;
+}
+
+
+//-- 使用HSV色表生成一个1x180的色表，用于对应0-360度的旋转
+cv::Mat OrientationColor::generateOrientationMap()
+{
+    cv::Mat hsvTabel(1,180,CV_8UC3,cv::Scalar::all(0));
+    for(int i = 0; i < 180; i++){
+        hsvTabel.at<cv::Vec3b>(0,i) = cv::Vec3b(i,255,255);
+    }
+    cv::cvtColor(hsvTabel, hsvTabel, cv::COLOR_HSV2BGR);
+    return hsvTabel;
 }
--- pseudoColor.h
+++ pseudoColor.h
@@ -1,3 +1,5 @@
+#ifndef PSEUDO_COLOR_H
+#define PSEUDO_COLOR_H
 #include <opencv2/opencv.hpp>
 #include <sophus/se3.hpp>
 #include <boost/format.hpp>
@@ -20,4 +22,41 @@ namespace PseudoColor{
     //-- 生成一个伪彩色的colorBar, 大小为 256x1
     cv::Mat generateColorMap(cv::ColormapTypes mapType);
 
-}
+}//namespace PseudoColor
+
+namespace OpticFlowColor{
+
+    /**
+     * @brief 可视化一个边长(radius*2+1)正方形的光流颜色映射图，将光流(x,y)映射为颜色m
+     * @param radius 可视化的最大光流的长，即map大小为 (radius*2+1)x(radius*2+1),
+     *               此时默认两个像素的长度一定小于radius（如EDF算出来最大200，那radius可取200）
+     * @details (x,y)对应的颜色矩阵的位置即(center+x, center+y), 其中center坐标为[radius,radius]
+     *          矩阵满足：不同朝向是不同颜色，HSV的H通道顺着360度，距离越大则饱和度越高，亮度始终最高
+    */
+    cv::Mat generatePixel2DViewMap(int radius);
+
+    /**
+     * @brief 根据最大的光流模长生成一个二维向量的颜色
+     * @param pixel 二维向量，即光流的[dx,dy]
+     * @param maxRadius 最大模，最大模的颜色为满饱和度满亮度
+    */
+    cv::Vec3b pixel2valueHSV(cv::Vec2f pixel, float maxRadius);
+
+    /**
+     * @brief 使用HSV伪彩色渲染一个二维光流图
+     * @param matPixel2D CV_32FC2的图像，每个[x,y]位置的Vec2f代表一个光流向量
+     *        [0]是图像坐标系x方向的偏移量
+     *        [1]是图像坐标系y方向的偏移量
+    */
+    cv::Mat visualizePixel2DMap(cv::Mat matPixel2D);
+
+}//namespace OpticFlowColor
+
+namespace OrientationColor{
+
+    //-- 使用HSV色表生成一个1x180的色表，用于对应0-360度的旋转向量
+    cv::Mat generateOrientationMap();
+
+}//-- namespace OrientationColor
+
+#endif
